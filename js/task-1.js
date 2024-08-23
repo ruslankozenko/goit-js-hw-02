7ebfee8c763d9571e578cd01f23bc09b0072c5f9
ruslankozenko <kozenko.r@seznam.cz>
2024-08-23 15:53:44 +0200

    task3 off

--- js/task-1.js
+++ js/task-1.js
@@ -3,13 +3,11 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
 
     let totalPrice = quantity * pricePerDroid;
-
+    
     if (totalPrice > customerCredits) {
-        
         return "Insufficient funds!";
 
     } else {
-
         return `You ordered ${quantity} droids worth ${totalPrice} credits!`
     }
 }
