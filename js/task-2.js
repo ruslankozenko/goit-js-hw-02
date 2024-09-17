0e740416992d58150482a9ab422322da7feef985
Ruslan Kozenko <kozenko.r@seznam.cz>
2024-09-17 10:49:13 +0200

    correction

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function formatMessage(message, maxLength) {
-
-    if (message.lenght <= maxLength) {
+    if (message.length <= maxLength) {
         return message;
     } else {
         return message.slice(0, maxLength) + "...";
